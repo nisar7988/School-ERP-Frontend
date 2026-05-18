b6fb267ba78c3279d941269168c766214ff84297
Nisar Ahmed <nisarcodeinit@gmail.com>
2026-05-18 17:10:44 +0530

    push with updated BE ui

--- src/lib/http/client.ts
+++ src/lib/http/client.ts
@@ -4,7 +4,9 @@ import { toast } from '@/lib/stores/toast.store'
 
 // Base API configuration
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://192.168.1.208:3000/api',
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    'https://school-erp-backend-2av9.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
